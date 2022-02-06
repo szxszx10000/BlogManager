64eadc50b3bc637a64c7c1aaac53736f3b93ef4d
shenzaixing <zaixing.shen@transwarp.cn>
2022-02-06 21:52:10 +0800

    创建后端工程

---
+++ vblog-java/src/main/java/com/nju/vblog/VblogJavaApplication.java
@@ -0,0 +1,13 @@
+package com.nju.vblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VblogJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VblogJavaApplication.class, args);
+    }
+
+}
